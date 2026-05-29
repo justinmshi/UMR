8739e5f96a53e0671d5c52fb7a9ef2f0947ba068
Justin Shi <justinmshi@icloud.com>
2026-05-29 13:48:59 -0700

    add audio playing from cube in Example

---
+++ Example/Assets/Scripts/Cube.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Renderer))]
+public class Cube : MonoBehaviour
+{
+  private const int PositionPeriod = 5;
+
+  private void Awake()
+  {
+    GetComponent<Renderer>().material.color = Color.red;
+  }
+
+  private void Update()
+  {
+    transform.Rotate(50 * Time.deltaTime * Vector3.right);
+
+    transform.position = new(
+      15 * Mathf.Sin(2 * Mathf.PI * Time.realtimeSinceStartup / PositionPeriod),
+      transform.position.y,
+      transform.position.z
+    );
+  }
+}
